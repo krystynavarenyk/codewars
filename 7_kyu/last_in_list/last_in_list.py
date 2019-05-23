e80280fe10786d6ca12b871bf576be3c768ca7e5
Kristina <kristinkavarenyk@gmail.com>
2019-05-23 22:07:58 +0200

    Finished last in list 7kyu kata.

---
+++ 7_kyu/last_in_list/last_in_list.py
@@ -0,0 +1,12 @@
+""" Krystyna Varenyk 2019
+
+My solution for 7 kyu kata:
+https://www.codewars.com/kata/99-problems-number-1-last-in-list/python
+
+"""
+
+def last(lst):
+    if lst:
+        return lst[-1]
+    else:
+        return None
